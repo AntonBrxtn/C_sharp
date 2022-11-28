751a533b22d8927a2d30f5b2b1e8dc7df782a09f
Anton Che <anton.chebykin@gmail.com>
2022-11-28 20:25:43 +1000

    задача с условием

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "григорий") 
+{
+    Console.WriteLine("Hi, Greg! I'm glad to see you!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
